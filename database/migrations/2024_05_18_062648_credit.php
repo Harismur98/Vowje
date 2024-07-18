b799ff52ce3b2ca78177b42bffa1f6bb0fd25bc2
harismur98 <harismur98@gmail.com>
2024-07-18 23:17:08 +0800

    make table small letter

--- database/migrations/2024_05_18_062648_credit.php
+++ database/migrations/2024_05_18_062648_credit.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Credits', function (Blueprint $table) {
+        Schema::create('credits', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('shop_id');
             $table->foreign('shop_id')->references('id')->on('shops');
--- database/migrations/2024_05_18_064448_transaction.php
+++ database/migrations/2024_05_18_064448_transaction.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Transactions', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('credit_id');
-            $table->foreign('credit_id')->references('id')->on('Credits');
+            $table->foreign('credit_id')->references('id')->on('credits');
             $table->float('amount');
             $table->string('status');
             $table->string('payment_method');
